293ab07f3ff585edd3898f5d74de7a0cdd797767
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-05-23 17:39:27 -0700

    Completed initial configuration and added example episode data

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,6 +3,16 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Episode from './../Episode';
 
+const testEpisode = {
+    id: 9801,
+    name: 'Test Episode',
+    image: 'http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg',
+    season: 9801,
+    number: 9801,
+    summary: 'This is a test summary.',
+    runtime: 9801
+}
+
 test("renders without error", () => { });
 
 test("renders the summary test passed as prop", () => { });
